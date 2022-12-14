f6ae1500d22bb8e30cade0046e2389d4b1641b4e
Jean <etnpfj@gmail.com>
2022-12-14 17:27:11 +0900

    로그인 구현하려했다가 약간 망한듯..

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const MongoClient = require('mongodb').MongoClient;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }))
 
 var db;
 
@@ -26,7 +34,41 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }), (req, res) => {
+    res.render('index.ejs')
+})
 app.get('/join', (req, res) => {
     res.render('join.ejs')
 })
+app.post('/join', (req, res) => {
+    db.collection('member').insertOne({ id: req.body.id, pw: req.body.pw, gender: req.body.gender, age: req.body.age })
+    res.redirect('/')
+})
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('member').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+        if (에러) return done(에러)
 
+        if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+        if (입력한비번 == 결과.pw) {
+            return done(null, 결과)
+        } else {
+            return done(null, false, { message: '비번틀렸어요' })
+        }
+    })
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id)
+});
+
+passport.deserializeUser(function (아이디, done) {
+    db.collection('member').findOne({ id: 아이디 }, function (에러, 결과) {
+        done(null, 결과)
+    })
+}); 
